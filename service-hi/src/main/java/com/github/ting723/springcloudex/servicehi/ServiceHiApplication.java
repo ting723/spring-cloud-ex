011d178398cf0908f4c3889ba62cf81aaf74a043
lianwei.zhang <lianwei.zhang@bitmain.com>
2018-11-29 18:19:16 +0800

    更新

--- service-hi/src/main/java/com/github/ting723/springcloudex/servicehi/ServiceHiApplication.java
+++ service-hi/src/main/java/com/github/ting723/springcloudex/servicehi/ServiceHiApplication.java
@@ -1,27 +1,70 @@
 package com.github.ting723.springcloudex.servicehi;
 
+import brave.sampler.Sampler;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @SpringBootApplication
 @EnableEurekaClient
 @RestController
+@EnableHystrix
+@EnableHystrixDashboard
+@EnableCircuitBreaker
 public class ServiceHiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ServiceHiApplication.class, args);
+    }
+    private static final Logger LOG = Logger.getLogger(ServiceHiApplication.class.getName());
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Bean
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
     }
 
     @Value("${server.port}")
     String port;
 
     @RequestMapping("/hi")
+    @HystrixCommand(fallbackMethod = "hiError")
     public String home(@RequestParam(value = "name",defaultValue = "forezp")String name){
         return "hi " +name +", i am from port:"+port;
+    }
+
+    @RequestMapping("/newhi")
+    public String callHome(){
+        LOG.log(Level.INFO, "calling trace service-hi  ");
+        return restTemplate.getForObject("http://localhost:8989/miya", String.class);
+    }
+    @RequestMapping("/info")
+    public String info(){
+        LOG.log(Level.INFO, "calling trace service-hi ");
+        return "i'm service-hi";
+    }
+    @Bean
+    public Sampler defaultSampler() {
+        return Sampler.ALWAYS_SAMPLE;
+    }
+
+    public String hiError(String name) {
+        return "hi,"+name+",sorry,error!";
     }
 }
